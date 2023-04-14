2ad4ebd71b8592cc88b967013cb6673caa02a761
Evan Haston <ehaston@darkmatterit.io>
2023-04-14 17:35:02 -0400

    feature: adding basic code

--- policy_ingestion/app.py
+++ policy_ingestion/app.py
@@ -1,42 +1,22 @@
 import json
+import boto3
 
 # import requests
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+  # Put event into event bridge with source laji_project.policydata
+  events = boto3.client('events')
+  response = events.put_events(
+        Entries=[
+            {
+                'Source': 'laji_project.policydata',
+                'DetailType': 'policydata',
+                'Detail': json.dumps(event),
+                'EventBusName': 'PolicyEventBus'
+            },
+        ]
+    )
+  return response
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
