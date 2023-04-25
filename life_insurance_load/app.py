141ba9f9127e9301f7efa6c5af36bc6abff2bd98
Evan Haston <ehaston@darkmatterit.io>
2023-04-24 20:57:07 -0400

    feature: adding life insurance load

--- life_insurance_load/app.py
+++ life_insurance_load/app.py
@@ -1,42 +1,45 @@
 import json
+import os
 
-# import requests
-
+import boto3
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    # Get items from sqs in batch of 10
+    sqs = boto3.client('sqs')
+    queue_url = os.getenv('SQS_QUEUE')
+    sqs_message = sqs.receive_message(
+        QueueUrl=queue_url,
+        AttributeNames=[
+            'SentTimestamp'
+        ],
+        MaxNumberOfMessages=10,
+        MessageAttributeNames=[
+            'All'
+        ],
+        VisibilityTimeout=15,
+        WaitTimeSeconds=10
+    )
+    # put items into dynamodb
+    dynamodb = boto3.client('dynamodb')
+    if 'Messages' in sqs_message:
+        for message in sqs_message['Messages']:
+            message_body = message['Body']
+            message_parsed = json.loads(message_body)
+            dynamodb.put_item(
+                TableName='PolicyData',
+                Item={
+                    'PolicyNumber': {'S': message_parsed['PolicyNumber']},
+                    'PolicyData': {'S': message_body}
+                }
+            )
+            # Delete the message from the queue
+            receipt_handle = message['ReceiptHandle']
+            sqs.delete_message(
+                QueueUrl=queue_url,
+                ReceiptHandle=receipt_handle
+            )
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
 
-    context: object, required
-        Lambda Context runtime methods and attributes
 
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
 
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
